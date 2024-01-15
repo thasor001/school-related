468a80e07760456a869ed23ae8d957565534f8a1
Dr.Terror <94102479+thasor001@users.noreply.github.com>
2024-01-15 22:48:47 +0100

    for shape, vx, vy in shapes: works but is a little ugly.

--- Assignments/pygletTest.py
+++ Assignments/pygletTest.py
@@ -2,19 +2,17 @@ import pyglet
 
 windowX, windowY = 1280, 720
 
-
 window = pyglet.window.Window(windowX, windowY, caption='PygletTest')
 
 shapes = []
 
-shapes.append(pyglet.shapes.Circle(200, 200, 50, color=(0, 255, 255)))
-shapes.append(pyglet.shapes.Circle(500, 500, 100, color=(0, 0, 255)))
-
+shapes.append([pyglet.shapes.Circle(200, 200, radius=100, color=(255, 0, 0)), 1, 1])
+shapes.append([pyglet.shapes.Circle(100, 100, radius=50, color=(0, 255, 255)), 1, 1])
 
 
 @window.event
 def on_mouse_drag(mx, my, dx, dy, buttons, modifiers):
-    for shape in shapes:
+    for shape, vx, vy in shapes:
         if (mx, my) in shape:
             motionX = abs(abs(shape.x) - abs(mx))
             motionY = abs(abs(shape.y) - abs(my))
@@ -26,15 +24,18 @@ def on_mouse_drag(mx, my, dx, dy, buttons, modifiers):
 @window.event
 def on_draw():
     window.clear()
-    for shape in shapes:
-        vx, vy = 1, 1
-        if shape.x >= windowX-shape.radius or shape.x <= 0-shape.radius:
+    index = 0
+    for shape, vx, vy in shapes:
+        if shape.x >= windowX-shape.radius or shape.x <= 0+shape.radius:
             vx *= -1
-        elif shape.y >= windowY-shape.radius or shape.y <= 0-shape.radius:
+        elif shape.y >= windowY-shape.radius or shape.y <= 0+shape.radius:
             vy *= -1
-        shape.x += 1 * vx
-        shape.y += 1 * vy
+        shape.x += (1 * vx)
+        shape.y += (1 * vy)
         shape.draw()
+        shapes[index][1] = vx
+        shapes[index][2] = vy
+        index += 1
 
 
 pyglet.app.run()
