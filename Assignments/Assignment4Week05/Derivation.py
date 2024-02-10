95ef1c5a84ce492ed7248e2026eda9bc6b96847b
Dr.Terror <94102479+thasor001@users.noreply.github.com>
2024-02-10 17:23:24 +0100

    GG

--- Assignments/Assignment4Week05/Derivation.py
+++ Assignments/Assignment4Week05/Derivation.py
@@ -1,20 +1,22 @@
 from sympy import *
 from numpy import array
 
-t = symbols("t")
+t, B, A, C, D = symbols("t B A C D")
 
 expr = t**3 + 8*t
 
 # print((expr.subs("t", 5.3)-expr.subs("t", 4.7))/0.6)
 
+
 def numDer(f, t, dt):
     return N((f.subs("t", t+dt) - f.subs("t", t-dt))/(2*dt))
+
 
 def vecDer(vec):
     return [diff(v, t) for v in vec]
 
 
-vec = array([cos(t), t**2])
+vec = array([t*cos(t**2), t**2])
 
 # print(vecDer(vec))
 
@@ -29,3 +31,16 @@ expr3 = E ** t
 expr4 = sin(t) + cos(t)
 
 # print(numDer(expr4, 2, 0.1))
+
+
+vec1 = array([t**2-t, t**4+3*t])
+
+# print(vecDer(vec1))
+
+# print(vecDer(vecDer(vec1)))
+
+expr5 = A*(1-t)**3+3*B*((1-t)**2)*t+3*C*(1-t)*t**2+D*t**3
+
+ph = diff(expr5, t)
+
+print(ph.subs("t", 0))
