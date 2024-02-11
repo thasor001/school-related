ad8a39589e9b0417d2c8b2c3d6f72e26c0172b7b
Dr.Terror <94102479+thasor001@users.noreply.github.com>
2024-02-11 23:20:28 +0100

    GG

--- Assignments/Assignment4Week05/bezierCurve.py
+++ Assignments/Assignment4Week05/bezierCurve.py
@@ -1,7 +1,21 @@
 from pyglet import *
 
 
+def posUptade(shape, dt):
+    shape.t += dt * shape.vel
+    if not shape.ret and shape.t > 1:
+        shape.t = 0
+    else:
+        if shape.t > 1 or shape.t < 0:
+            shape.vel *= -1
+            shape.t += dt * shape.vel
+
+
 class Circle(shapes.Circle):
+    t = 0
+    vel = 1
+    ret = False
+
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.radius = radius
@@ -11,36 +25,44 @@ class Circle(shapes.Circle):
         self.color = (0, 0, 255, 255)
 
     @classmethod
-    def new_pos(cls, x, y):
-        pass
+    def updateTime(cls, dt):
+        for shape in window.items:
+            if isinstance(shape, list):
+                for obj in shape:
+                    posUptade(obj, dt)
+            else:
+                posUptade(shape, dt)
 
-
-
-class Square(shapes.Rectangle):
-    def __init__(self, x, y, height, width):
-        super().__init__(x, y, height, width)
-        self.height = height
-        self.width = width
-        self.x = x
-        self.y = y
-        self.color = (0, 255, 0, 255)
-        self.batch = window.batch
 
 
 class MyWindow(window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.batch = graphics.Batch()
-        self.time = 0
         self.items = []
+        self.cube = []
 
-    def on_draw(self):
+    def cubeBezier(self, start, control, end, t) -> None:
+        pass
+
+    def update(self, dt) -> None:
+        for obj in self.cube:
+            self.cubeBezier([window.width, window.height-100],
+                            [window.width, 0],
+                            [20, 350], obj.t)
+
+        Circle.updateTime(dt)
+
+    def on_draw(self) -> None:
         self.clear()
         self.batch.draw()
 
+
 if __name__ == "__main__":
     window = MyWindow(height=500, width=500, caption="Bezier Curve")
-    window.items.append(Circle(x=100, y=100, radius=25))
-    window.items.append(Square(x=350, y=350, height=50, width=50))
+    window.items.append(Circle(window.width, window.height-100, 25))
 
-app.run()
+    clock.schedule_interval(window.update, 1/60.)
+
+    window.items.append(window.cube)
+    app.run()
