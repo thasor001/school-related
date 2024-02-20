058d07cdc29b7fa1f207711fe3c8324a179fbdd8
Dr.Terror <94102479+thasor001@users.noreply.github.com>
2024-02-20 19:50:21 +0100

    i am now a livid enjoyer of std:: Oblig 2 DONE!

--- OBLIG/Oblig2.cpp
+++ OBLIG/Oblig2.cpp
@@ -1,28 +1,34 @@
 //
 // Created by Tharald on 19/02/2024.
+// Finished 20/02/2024.
 //
 
 #include "LesData2.h"
+// Header file containing all the classes.
 #include "Oblig2Classes.h"
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
 
 void menu() {
-    cout << "F - Fish " << "\t\tI - Bug \n"
+    std::cout << "F - Fish " << "\t\tI - Bug \n"
     << "C - Crustacean" << "\t\tB - Bird\n"
     << "Q - End Program " << "\tW - Write All\n\n";
 }
 
-vector <Dyr*> gDyr;
+///< Vector containing Dyr pointers
+std::vector <Dyr*> gDyr;
 
+/**
+ * Main program.
+ */
 int main() {
+    // Class pointers for different animals
     Fisk * newFish;
     Innsekt * newBug;
     Fugl * newBird;
     Skalldyr * newCrab;
-    string name;
+    std::string name;
 
     menu();
     char command = lesChar("\nWrite command ");
@@ -30,7 +36,11 @@ int main() {
     while (command != 'Q') {
         switch (command) {
             case 'B':
+                // Creating a new instance of Class.
                 newBird = new Fugl;
+                // Pushing this instance to the back of gDyr vector.
+                // This instance can be pushed into a vector with pointers to Dyr even though,
+                // it isn't a Dyr class, but it is a subclass of Dyr.
                 gDyr.push_back(newBird);
                 break;
             case 'I':
@@ -38,12 +48,15 @@ int main() {
                 gDyr.push_back(newBug);
                 break;
             case 'F':
-                cout << "Name : "; getline(cin, name);
-                if (name != "\n") {
-                    newFish = new Fisk(name);
+                std::cout << "Name : ";
+                getline(std::cin, name);
+                // using name.empty instead of == '\n' since getline reads until '\n' then,
+                // removes it, so therefore == '\n' wouldn't work since it is not stored in name.
+                if (name.empty()) {
+                    newFish = new Fisk;
                     gDyr.push_back(newFish);
                 } else {
-                    newFish = new Fisk;
+                    newFish = new Fisk(name);
                     gDyr.push_back(newFish);
                 }
                 break;
@@ -52,21 +65,27 @@ int main() {
                 gDyr.push_back(newCrab);
                 break;
             case 'W':
+                // Go through all gDyr pointers and writeData, this does not just write their name,
+                // since the writeData in Dyr class is a virtual and is overridden by the writeData in,
+                // the subclasses in "Fisk, Fugl, Innsekt and Skalldyr", which means it will run writeData,
+                // of the subclasses.
                 for (auto & dyr : gDyr) {
                     dyr->writeData();
                 }
                 break;
-            default: cout << "Illegal Command : " << command << "\n"; break;
+            default: std::cout << "Illegal Command : " << command << "\n"; break;
         }
-        cout << "\nF->Fish ; I->Bug ; C->Crab ; B->Brid ; W->Write";
+        std::cout << "\n| F->Fish | I->Bug | C->Crab | B->Bird | W->Write |";
         command = lesChar("\nWrite command ");
     }
 
+    // Goes through all of gDyr and works with the originals in the vector, then deletes.
     for (auto & val : gDyr) {
         delete val;
     }
+    // Finally clears the vector.
     gDyr.clear();
-    cout << "\n\t--Program Terminated--\n";
+    std::cout << "\n\t--Program Terminated--\n";
 
     return 0;
 }
