753dcb55ba8d1793c9ebfd7c9eb8fb4cf99fd5e5
Dr.Terror <94102479+thasor001@users.noreply.github.com>
2024-02-21 17:20:26 +0100

    Task 12 done with some exstra stuff. :)

--- WeekTasks/Task12.cpp
+++ WeekTasks/Task12.cpp
@@ -1,28 +1,38 @@
 //
 // Created by Tharald on 2/21/2024.
 //
+
+// No need to include things such as <iostream> as they are already inside LesData2.h
 #include "LesData2.h"
 #include <vector>
 #include <string>
 
+/**
+ * Customer class
+ */
 class Customer {
 private:
     std::string name;
+    ///< Vector with float's pointing to accounts.
     std::vector <float>* accounts;
 public:
     Customer(const int n) : accounts(new std::vector <float>()){
+        // Line above allocates space for an "accounts" vector.
         for (int i = 0; i < n; i++) {
+            // Fills vector up with 0.0
             accounts->push_back(0.0);
         }
-        std::cout << "--Created : " << n << " Accounts--\n";
+        std::cout << "\n--Created : " << n << " Accounts--\n";
         Customer::readData();
     }
     ~Customer() {
         std::cout << "\t-Deleted Customer : -" << name
         << "- And " << accounts->size() << " Associated Accounts" << "\n";
+        ///< Clears the vector (--Unsure if this will create a memory leak--)
         accounts->clear();
     }
-    bool getName(std::string name) {
+    // Getname function that compares names (sends in const of original).
+    bool getName(const std::string & name) {
         return (this->name == name);
     }
 
@@ -30,13 +40,16 @@ public:
         std::cout << "\nAccount Owner Name : ";
         std::getline(std::cin, name);
         for (int i = 0; i < accounts->size(); i++) {
-            std::cout << "Account Number : " << i+1;
-            (*accounts)[i] = lesFloat("Write Amount $ in Account ", 1, 1000);
+            std::cout << "Account Number " << i+1;
+            // since we currently are working with a pointer to accounts, we need to go into that pointer,
+            // we do that by pointing into what the pointer points to. **
+            (*accounts)[i] = lesFloat(" | Write Amount $ in Account ", 1, 1000);
         }
     }
     void writeData() {
         float sum = 0;
         std::cout << "\nAccount Owner : " << name << "\n";
+        // to work with the original values inside accounts, we need to de-reference it.
         for (auto & val : *accounts) {
             sum += val;
         }
@@ -50,8 +63,9 @@ void menu() {
     << "\tW - Write all customers"
     << "\tD - Delete customer by name\n\n";
 }
-int findName(std::string name);
+int findName(const std::string & name);
 
+///< Vector with Customer class pointers.
 std::vector <Customer*> gCustomers;
 
 int main() {
@@ -71,33 +85,46 @@ int main() {
                 break;
             case 'W':
                 for (auto & customer : gCustomers) {
+                    // Since the "Customer" that is inside gCustomers are pointers, we go inside it with "->".
                     customer->writeData();
                 }
                 break;
             case 'D':
                 std::cout << "\nWrite Name : ";
                 std::getline(std::cin, name);
-                if ((n = findName(name)) == -1) {
+                // We don't care about keeping what's inside name after findName function,
+                // Therefore we can change its value from Ivalue to Xvalue using std::move(),
+                // which should result in efficient transfer with no unnecessary copying.
+                // Overkill.
+                if ((n = findName(std::move(name))) == -1) {
                     std::cout << "Customer Does Not Exist\n";
-                    break;
+                } else {
+                    // First we delete the pointer that contains the customer.
+                    delete gCustomers[n];
+                    // Then we delete the spot that the deallocated customer was at.
+                    // .erase function erases the spot and automatically shoves the elements in front,
+                    // to the back, .erase takes an iterator "gCustomer.begin()" and then the element to go to "n".
+                    gCustomers.erase(gCustomers.begin() + n);
                 }
-                delete gCustomers[n];
                 break;
             default:menu(); std::cout << "Illegal command : " << command << "\n"; break;
         }
 
-        command = lesChar("Write command ");
+        command = lesChar("\nWrite command ");
     }
 
+    // De-allocates all pointers in gCustomers.
     for (auto & val : gCustomers) {
         delete val;
     }
+    // Clears all the de allocated pointers and reserved memory in the vector.
     gCustomers.clear();
 
     return 0;
 }
 
-int findName(std::string name) {
+// Same as ever.
+int findName(const std::string & name) {
     for (int i = 0; i < gCustomers.size(); i++) {
         if(gCustomers[i]->getName(name)) {
             return i;
