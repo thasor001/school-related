0e71e5f0b351a388575e7b2b6d8cc14a3772cf38
Dr.Terror <94102479+thasor001@users.noreply.github.com>
2024-03-19 10:28:22 +0100

    GG!

--- Assignments/Assignment7/Task8.py
+++ Assignments/Assignment7/Task8.py
@@ -1,49 +1,58 @@
 from pyglet import *
 import numpy as np
 from random import uniform
-from time import time
-from math import sqrt
 
-window = window.Window(height=1000, width=1000)
+window = window.Window(height=750, width=1000)
 
 n = 2
 
 disks = np.array([np.array([
-        uniform(100, window.height-100),  # X
-        uniform(100, window.width-100),  # Y
+        uniform(175, window.width-175),  # X
+        uniform(175, window.height-175),  # Y
     ])for _ in range(n)])
 
 disp = disks.reshape(n, 1, 2) - disks
 dist = np.sqrt(np.sum(disp**2, axis=2))
 
+weights = np.array([uniform(5, 10)for _ in range(n)])
+vel = np.zeros((n, 2))
+
+print("_______X_____________Y_______ DISKS")
 print(disks)
-print("_________________--__________ Vector")
+print("_______________1_____________ Vector DISP")
 print(disp)
-print("_________________--__________ Distance")
+print("_______1_____________2_______ Distance DIST")
 print(dist)
+print("_______1_____________2_______ Weight")
+print(weights)
+print("_______1_____________2_______ Vel")
+print(vel)
+print("_____________________________ NEW VEL")
+test = np.zeros((n, 2))
 
-weight = np.array([uniform(5, 10)for _ in range(n)])
-vel = np.array([np.array([0., 0.])for _ in range(n)])
+for i in range(n):
+    vel[i, :] = ((0.2 * 50) * weights[weights != weights[i]] / (dist[i, dist[i] != 0] ** 2)) * disp[i, disp[i] != 0][0:2]
 
+print(vel)
 
+for i in range(n):
+    test[0, :] += ((0.2*50) * weights[1] / (dist[0, 1]**2)) * disp[0, 1]
+
+print(test)
 
 def gravity(dt):
-    pass
-
+    global vel, weights, dist
+    for i in range(n):
+        #vel[i, :] = (dt*20 * weights[weights != weights[i]] / (dist[i, dist[i] != 0]**2)) * disp[i, disp[i] != 0][0:2]
+        pass
 
 
 @window.event()
 def on_draw():
     window.clear()
+    for i in range(n):
+        shapes.Circle(x=disks[i][0], y=disks[i][1], radius=weights[i], color=(255, 0, 0)).draw()
 
-
-start = time()
-disp2 = (10, 2)
-for _ in range(1):
-    pass
-
-end = time()
-#print(end - start)
 
 clock.schedule_interval(gravity, 1/60)
 app.run()
