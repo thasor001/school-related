5b54807f0d50164d8fcd96557869ee773d6688a1
thasor001 <tharalrs@stud.ntnu.no>
2024-03-19 16:06:22 +0100

    Beginning of circle, line collisions

--- Assignments/Assignment7/Task8.py
+++ Assignments/Assignment7/Task8.py
@@ -4,17 +4,21 @@ from random import uniform
 
 window = window.Window(height=750, width=1000)
 
-n = 2
+n = 4
 
 disks = np.array([np.array([
         uniform(175, window.width-175),  # X
         uniform(175, window.height-175),  # Y
     ])for _ in range(n)])
 
-disp = disks.reshape(n, 1, 2) - disks
+x_mesh, y_mesh = np.meshgrid(disks[:, 0], disks[:, 1])
+
+disp = np.dstack((x_mesh - x_mesh.T, y_mesh - y_mesh.T))
 dist = np.sqrt(np.sum(disp**2, axis=2))
 
-weights = np.array([uniform(5, 10)for _ in range(n)])
+weight = np.array([uniform(5, 10)for _ in range(n)])
+weights = np.meshgrid(weight, weight)
+weights = weights[0] * weight[1]
 vel = np.zeros((n, 2))
 
 print("_______X_____________Y_______ DISKS")
@@ -27,32 +31,21 @@ print("_______1_____________2_______ Weight")
 print(weights)
 print("_______1_____________2_______ Vel")
 print(vel)
-print("_____________________________ NEW VEL")
-test = np.zeros((n, 2))
-
-for i in range(n):
-    vel[i, :] = ((0.2 * 50) * weights[weights != weights[i]] / (dist[i, dist[i] != 0] ** 2)) * disp[i, disp[i] != 0][0:2]
-
-print(vel)
-
-for i in range(n):
-    test[0, :] += ((0.2*50) * weights[1] / (dist[0, 1]**2)) * disp[0, 1]
-
-print(test)
 
 def gravity(dt):
-    global vel, weights, dist
-    for i in range(n):
-        #vel[i, :] = (dt*20 * weights[weights != weights[i]] / (dist[i, dist[i] != 0]**2)) * disp[i, disp[i] != 0][0:2]
-        pass
+    global vel, weights, dist, disks
+    vel = vel + np.sum((dt * -10 * weights)/(dist**2) * disp[:, :], axis=0)
+    disks[:, :] = disks[:, :] + vel[:, :] * dt
 
 
 @window.event()
 def on_draw():
     window.clear()
     for i in range(n):
-        shapes.Circle(x=disks[i][0], y=disks[i][1], radius=weights[i], color=(255, 0, 0)).draw()
-
+        if i == 0:
+            shapes.Circle(x=disks[i][0], y=disks[i][1], radius=weights[i, 0], color=(255, 0, 0)).draw()
+        else:
+            shapes.Circle(x=disks[i][0], y=disks[i][1], radius=weights[i, 0], color=(0, 0, 255)).draw()
 
 clock.schedule_interval(gravity, 1/60)
 app.run()
